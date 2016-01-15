328f26f818259c147e6975151e4aa28dc0358df9
RizOrt218 <rizhiafalen@gmail.com>
2016-01-15 00:19:55 -1000

    works, but doesnt rec special characters

--- server.js
+++ server.js
@@ -1,27 +1,42 @@
 var express    = require( 'express' );
 var bodyParser = require( 'body-parser' );
 var server     = express();
+
 var switchWords = '';
 
 server.use( express.static( 'public' ));
 server.use( bodyParser.json({ type: 'application/json' }) );
 
-function switcher (req, res, next) {
-  //switch words
-  switchWords = req.body.message
-    .replace( 'selfie', 'self-portrait' )
-    .replace( 'yummers', 'delicious')
-    .replace( 'outchea', 'are out here')
-    .replace( 'bruh', 'wow')
-    .replace( 'doge', 'pug')
-    .replace( 'cilantro', 'soap')
-    .replace( 'bae', 'loved one')
-    .replace( 'swag', 'style')
-    .replace( 'yolo', 'carpe diem');
+function zeSwitch (req, res, next) {
+  var sen = (req.body.message).toLowerCase();
+  console.log(sen);
+  var splitSen = sen.split(' ');
+  console.log(splitSen);
 
+  var ohNoYouDint = {
+    'selfie': 'self-portrait',
+    'yummers': 'delicious',
+    'outchea' : 'delicious',
+    'bruh' : 'wow',
+    'doge' : 'pug',
+    'cilantro' : 'soap',
+    'bae' : 'loved one',
+    'swag' : 'style',
+    'yolo' : 'carpe diem'
+  };
+
+  for( var key in ohNoYouDint ) {
+    for( var i = 0; i < splitSen.length; i++ ) {
+       if( key === splitSen[i] ){
+         splitSen[i] = ohNoYouDint[key];
+         console.log(splitSen);
+         switchWords = splitSen.join(' ');
+       }
+    }
+  }
   next();
 }
-server.use( switcher );
+server.use( zeSwitch );
 
 server.get('/', function (req, res) {
   res.render( './index.html' );
